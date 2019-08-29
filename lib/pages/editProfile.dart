713dd74676152ba20b4865d201d14afa7a089738
Trey <trey.a.hope@gmail.com>
2019-08-29 00:22:38 -0400

    Pushed to iOS and Android market.

--- lib/pages/editProfile.dart
+++ lib/pages/editProfile.dart
@@ -720,7 +720,8 @@ class EditProfilePageState extends State<EditProfilePage>
             SizedBox(height: 20),
             Text('Sub Category'),
             SizedBox(height: 20),
-            subCategoryDropdownField()
+            subCategoryDropdownField(),
+            Container()
           ],
         ),
       ),
