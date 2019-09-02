98508592ef090e0a72d553f782dd987c4d47b131
Trey <trey.a.hope@gmail.com>
2019-09-01 22:30:49 -0400

    Added Stripe func for Node.js temporarly.

--- web/functions/index.js
+++ web/functions/index.js
@@ -2,6 +2,7 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const algoliasearch = require('algoliasearch');
 const algoliaSync = require('algolia-firestore-sync');
+const stripe = require("stripe")("sk_test_7VBBm17Ry1fpkpa8u7w1esNA");
 
 const ALGOLIA_APP_ID = 'ZWB00DM8S2';
 const ALGOLIA_ADMIN_KEY = '5425be19aea3a951b40826e5549c03f1';
@@ -16,3 +17,24 @@ exports.updateGem = functions.firestore
         var index = client.initIndex(ALGOLIA_INDEX_NAME);
         return algoliaSync.syncAlgoliaWithFirestore(index, change, context);
     });
+
+/*
+    RETRIEVE A CHARGE
+
+    Retrieves the details of a charge that has previously been created. 
+    Supply the unique charge ID that was returned from your previous request, 
+    and Stripe will return the corresponding charge information. The same 
+    information is returned when creating or refunding the charge.
+
+    PARAMS
+
+    String chargeId;
+*/
+exports.retrieveCard = functions.https.onRequest((request, response) => {
+    const chargeId = request.query.chargeId;
+
+    return stripe.charges.retrieve(chargeId, (err, charge) => {
+        if (err) throw err;
+        response.status(200).send(charge);
+    });
+});
